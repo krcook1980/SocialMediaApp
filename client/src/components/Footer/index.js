42e54e1f53477b8a964e84d079e019775e9f2c6a
Kelly Cook <kellyrcook1980@gmail.com>
2021-07-01 16:04:54 -0600

    add front end

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;2021 Kelly Cook - code base by 2U</div>
+    </footer>
+  );
+};
+
+export default Footer;
